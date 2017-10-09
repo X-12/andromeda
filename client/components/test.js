8320ba82b3a88f4cf339a6f7b23fa2596fb60a31
Trevor Jex <jextrevor@gmail.com>
2017-10-09 16:38:29 -0600

    Add use strict to JS files

--- client/components/test.js
+++ client/components/test.js
@@ -1,3 +1,4 @@
+"use strict";
 const React = require('react');
 class Test extends React.Component {
     render(){
--- server/main.js
+++ server/main.js
@@ -1,3 +1,4 @@
+"use strict";
 const io = require('socket.io')();
 const fork = require('child_process').fork
 const webserver = fork('./server/webserver.js')
