743b05dc86e8512db06bf5f40f13bba116d27c94
Trevor Jex <jextrevor@gmail.com>
2017-10-10 21:35:29 -0600

    Fix code formatting

--- server/main.js
+++ server/main.js
@@ -3,6 +3,5 @@ const io = require('socket.io')();
 const fork = require('child_process').fork
 const webserver = fork('./server/webserver.js')
 const port = 3000
-io.listen(port, function(){
-    console.log("Socket.IO listening on port "+port)
-})
+io.listen(port)
+console.log("Socket.IO listening on port "+port)
