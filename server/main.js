1e0b80e150f770f52b56eca0c8864006e06d06da
Trevor Jex <jextrevor@gmail.com>
2017-10-10 21:34:36 -0600

    Improve semantics and console logging

--- server/main.js
+++ server/main.js
@@ -2,3 +2,7 @@
 const io = require('socket.io')();
 const fork = require('child_process').fork
 const webserver = fork('./server/webserver.js')
+const port = 3000
+io.listen(port, function(){
+    console.log("Socket.IO listening on port "+port)
+})
--- server/webserver.js
+++ server/webserver.js
@@ -2,11 +2,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
+const port = 8000
 app.get("/station1", function(req,res){
     res.sendFile(path.join(__dirname,"../client","station1/index.html"))
 })
 app.use(express.static('public'))
-app.listen(8000, function(){
-    console.log("Listening on port 8000")
+app.listen(port, function(){
+    console.log("Express listening on port "+port)
 })
