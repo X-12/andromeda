3a5d213f4a04e012c16f806630386523c164fc3c
Trevor Jex <jextrevor@gmail.com>
2017-10-14 19:36:10 -0600

    Update file structure

--- client/station2/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//import styles from "./style.scss"
-const React = require('react')
-const ReactDOM = require('react-dom')
-const io = require('socket.io-client')
-const Test = require('../components/test')("hello")
-
-ReactDOM.render(<Test />,document.getElementById("test"))
--- client/stations/station1.js
+++ client/stations/station1.js
@@ -3,5 +3,4 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const io = require('socket.io-client')
 const Shade = require('../components/shade')(null)
-import '../styles/main.scss'
-ReactDOM.render(<Shade />,document.getElementById("test"))
+ReactDOM.render(<Shade />,document.getElementById("react-root"))
