0117efb7ad3224bf5ab4fb78cb78c34b89f3d64a
Trevor Jex <jextrevor@gmail.com>
2017-10-14 20:13:35 -0600

    Fix reference

--- client/stations/station1.js
+++ client/stations/station1.js
@@ -2,5 +2,5 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const io = require('socket.io-client')
-const Shade = require('../components/shade')(null)
+const Shade = require('../components/layout/shade')(null)
 ReactDOM.render(<Shade />,document.getElementById("react-root"))
