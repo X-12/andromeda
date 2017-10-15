2d32471b5879d547cf7923a2d4737ad3b9a6f3c4
Trevor Jex <jextrevor@gmail.com>
2017-10-14 21:37:52 -0600

    Change layout of system

--- server/systems/lights.js
+++ server/systems/lights.js
@@ -1,23 +1,22 @@
 "use strict";
 
-class Lights {
-    constructor(io){
-        this.on = false
-        this.io = io
-        this.io.on("Lights.setLight", (value) => {
-            setLight(value)
-        })
-    }
-    setLight(value){
-        if(typeof value == "boolean"){
-            this.on = value
-            this.io.emit("Lights.on",this.on)
-        }
-        else{
-            console.error("Invalid type "+typeof value+" for Lights.setLight")
-        }
-    }
-}
 module.exports = (io) =>{
-    return new Lights(io)
+    class Lights {
+        constructor(){
+            this.on = false
+            io.on("Lights.setLight", (value) => {
+                setLight(value)
+            })
+        }
+        setLight(value){
+            if(typeof value == "boolean"){
+                this.on = value
+                io.emit("Lights.on",this.on)
+            }
+            else{
+                console.error("Invalid type "+typeof value+" for Lights.setLight")
+            }
+        }
+    }
+    return new Lights()
 }
