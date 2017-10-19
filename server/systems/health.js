3cfa1669dd64dde918ff97ca54c3ddc1d7365a8d
Trevor Jex <jextrevor@gmail.com>
2017-10-18 20:57:05 -0600

    Fix dumb errors

--- server/systems/health.js
+++ server/systems/health.js
@@ -1,11 +1,13 @@
 "use strict";
 
 module.exports = (io,Ship) => {
-    class Health {
+    const System = require("./system")(io,Ship)
+    class Health extends System {
         constructor(){
             super("Health")
             this.set("LifeSupport",100)
             this.set("Hull",100)
         }
     }
+    return new Health()
 }
