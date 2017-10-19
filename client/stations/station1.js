2849c283c8160a6c1e4e51101342561299126663
Trevor Jex <jextrevor@gmail.com>
2017-10-18 20:35:32 -0600

    Add station file for shared imports

--- client/stations/station1.js
+++ client/stations/station1.js
@@ -2,5 +2,6 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const io = require('socket.io-client')("localhost:3000")
+require('./station')
 const Shade = require('../components/layout/shade')(io)
 ReactDOM.render(<Shade />,document.getElementById("react-root"))
