0192520afb0972d3aaedcdef269c62f73f18153e
Trevor Jex <jextrevor@gmail.com>
2017-10-24 10:10:18 -0600

    Add placeholders for health and power

--- server/systems/warp.js
+++ server/systems/warp.js
@@ -9,7 +9,9 @@ module.exports = (io,Ship) =>{
         }
         setSpeed(value){
             if(typeof value == "number" && 0 <= value < 10){
+                //if(Ship.Power.Warp > some value && Ship.Health.Warp > some value){
                 this.set("speed",value)
+                //}
             }
             else{
                 console.error("Invalid type "+typeof value+" for Warp.setSpeed")
@@ -20,6 +22,25 @@ module.exports = (io,Ship) =>{
             socket.on("Warp.setSpeed", (value)=>{
                 this.setSpeed(value)
             })
+        }
+        powerChanged(){
+            /*
+            if(Ship.Power.Warp < some config value){
+                this.set("speed",0)
+            }
+            */
+        }
+        healthChanged(){
+            /*
+            if(Ship.Health.Warp < some config value){
+                this.set("speed",0)
+            }*/
+        }
+        setupWatches(){
+            /*
+            Ship.Power.watch("Warp",this.powerChanged)
+            Ship.Health.watch("Warp",this.healthChanged)
+            */
         }
     }
     return new Warp()
