83c29bd51bea2001e67f11075745c0684221973b
Trevor Jex <jextrevor@gmail.com>
2017-10-30 09:53:27 -0600

    Add sensors system

--- server/systems/sensors.js
+++ server/systems/sensors.js
@@ -5,13 +5,70 @@ module.exports = (io,Ship) =>{
     class Sensors extends System {
         constructor(){
             super("Sensors")
+            this.set("scanning","")
+            this.set("logs",[])
+            this.set("analysing","")
         }
-
+        startScan(type){
+            //if(Ship.Power.Sensors > some value && Ship.Health.Sensors > some value){
+                this.set("scanning",type)
+            //}
+        }
+        completeScan(result){
+            let newlogs = this.logs
+            newlogs.push({action:"scan",type:this.scanning,result:result})
+            this.set("logs",newlogs)
+            this.set("scanning","")
+        }
+        completeAnalyse(result){
+            let newlogs = this.logs
+            newlogs.push({action:"analyse",type:this.analysing,result:result})
+            this.set("logs",newlogs)
+            this.set("analysing","")
+        }
+        startAnalyse(type){
+            //if(Ship.Power.Sensors > some value && Ship.Health.Sensors > some value){
+                this.set("analysing",type)
+            //}
+        }
+        addLog(action,type,result){
+            let newlogs = this.logs
+            newlogs.push({action:action,type:type,result:result})
+            this.set("logs",newlogs)
+        }
         setupSocket(socket){
             super.setupSocket(socket)
+            socket.on("Sensors.startScan",(data)=>{
+                this.startScan(data)
+            })
+            socket.on("Sensors.startAnalyse",(data)=>{
+                this.startAnalyse(data)
+            })
+            socket.on("Sensors.completeScan",(data)=>{
+                this.completeScan(data)
+            })
+            socket.on("Sensors.completeAnalyse",(data)=>{
+                this.completeAnalyse(data)
+            })
+            socket.on("Sensors.addLog",(data)=>{
+                this.addLog(data.action,data.type,data.result)
+            })
         }
         setupWatches(){
-
+            //Ship.Power.watch("Sensors",this.powerChanged)
+            //Ship.Health.watch("Sensors",this.healthChanged)
+        }
+        powerChanged(){
+            /*if(Ship.Power.Sensors < some value){
+                this.set("scanning","")
+                this.set("analysing","")
+            }*/
+        }
+        healthChanged(){
+            /*if(Ship.Health.Sensors < some value){
+                this.set("scanning","")
+                this.set("analysing","")
+            }*/
         }
     }
     return new Sensors()
