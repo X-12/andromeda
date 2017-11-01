2da008a701d8423462b35f15aa3f4383b8562b17
Trevor Jex <jextrevor@gmail.com>
2017-11-01 10:32:11 -0600

    Add radar skeleton

---
+++ server/systems/radar.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = (io,Ship) =>{
+    const System = require("./system")(io,Ship)
+    class Radar extends System{
+        constructor(){
+            super("Radar")
+        }
+        setupSocket(){
+            super.setupSocket()
+        }
+        setupWatches(){
+            
+        }
+    }
+}
