0e2af251ee71d46b19be0f63344e259c508c4cbc
DakotaHarward <daktoa101@gmail.com>
2017-11-06 17:21:36 -0700

    Added more health and power values.

--- server/systems/health.js
+++ server/systems/health.js
@@ -5,8 +5,12 @@ module.exports = (io,Ship) => {
     class Health extends System {
         constructor(){
             super("Health")
+            this.set("Sensors", Ship.Defaults.Health.Sensors)
+            this.set("Impulse", Ship.Defaults.Health.Impulse)
+            this.set("Warp", Ship.Defaults.Health.Warp)
+            this.set("Communications", Ship.Defaults.Health.Communications)
             this.set("LifeSupport", Ship.Defaults.Health.LifeSupport)
-            this.set("Hull",Ship.Defaults.Health.Hull)
+            this.set("Hull", Ship.Defaults.Health.Hull)
         }
     }
     return new Health()
