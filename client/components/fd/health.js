552deea865659e80419b272fb3aae423fb405eeb
Trevor Jex <jextrevor@gmail.com>
2017-11-16 19:52:23 -0700

    Add health system

--- client/components/fd/health.js
+++ client/components/fd/health.js
@@ -1,2 +1,27 @@
 "use strict";
 const React = require('react');
+require('../../styles/fd/health.scss')
+module.exports = (io) => {
+    class HealthFD extends React.Component {
+        constructor(props){
+            super(props)
+            this.state = {}
+            this.values = ["Sensors","Impulse","Warp","Communications","LifeSupport","Radar","Targeting","Hull"]
+            this.values.forEach((value)=>{
+                this.state[value] = 0
+                io.on("Health."+value, (data)=>{
+                    this.setState({[value]:data})
+                })
+            })
+        }
+        render(){
+            return (
+                <div>
+                    <ul>
+                    this.values.map((item)=><li key={item}>{this.state[item]}</li>)
+                    </ul>
+                </div>
+            )
+        }
+    }
+}
--- client/stations/fd.js
+++ client/stations/fd.js
@@ -3,11 +3,12 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const io = require('socket.io-client')("localhost:3000")
 require('./station')
+const Health = require('../components/fd/health')
 class ReactRoot extends React.Component {
     render(){
         return (
             <div>
-
+                <Health />
             </div>
         )
     }
