0afdf54af81cff78422cee01220159d4e0c16156
Trevor Jex <jextrevor@gmail.com>
2017-11-24 10:57:24 -0700

    Add alert changing functionality

--- client/components/systems/alert.js
+++ client/components/systems/alert.js
@@ -12,10 +12,16 @@ module.exports = (io) => {
                 this.setState({status: data})
             })
         }
+        setStatus(status){
+            io.emit("Alert.setStatus",status)
+        }
         render(){
             return (
                 <div className={styles[statuses[this.state.status]]}>
                     <h3>Alert Status: {this.state.status}</h3>
+                    <button className={styles.red} onClick={()=>this.setStatus(1)}>Red</button>
+                    <button className={styles.yellow} onClick={()=>this.setStatus(2)}>Yellow</button>
+                    <button className={styles.green} onClick={()=>this.setStatus(3)}>Green</button>
                 </div>
             )
         }
