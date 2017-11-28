d482119bdbb6f9a3ec001bff1edcccbdd6a0a335
Trevor Jex <jextrevor@gmail.com>
2017-11-28 10:47:15 -0700

    Create life support thread

--- server/systems/lifesupport.js
+++ server/systems/lifesupport.js
@@ -30,6 +30,26 @@ module.exports = (io,Ship) =>{
                 console.error("Invalid type "+typeof value+" for LifeSupport.setStatus")
             }
         }
+        updateOxygen(){
+            let delta = this.getDelta()/1000
+            if(this.status == true){
+                if(this.oxygen < 100){
+                this.setT("oxygen",this.oxygen+Ship.Defaults.LifeSupport.replenishRate*delta)
+                }
+                else{
+                    this.setT("oxygen",100)
+                }
+            }
+            else{
+                if(this.oxygen > 0){
+                    this.setT("oxygen",this.oxygen-Ship.Defaults.LifeSupport.depletionRate*delta)
+                }
+                else{
+                    this.setT("oxygen",0)
+                }
+            }
+            setImmediate(()=>{this.updateOxygen()})
+        }
         setupSocket(socket){
             super.setupSocket(socket)
             socket.on("LifeSupport.setStatus", (value) => {
@@ -39,6 +59,7 @@ module.exports = (io,Ship) =>{
         setupWatches(){
             Ship.Power.watch("LifeSupport",Ship.LifeSupport.powerChanged)
             Ship.Health.watch("LifeSupport",Ship.LifeSupport.healthChanged)
+            this.updateOxygen()
         }
         powerChanged(){
             
