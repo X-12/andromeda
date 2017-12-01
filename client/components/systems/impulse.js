8b2b5187417f747e7c079335c6136f6d32b8a1e0
Trevor Jex <jextrevor@gmail.com>
2017-11-30 18:12:27 -0700

    Fix classname attribute

--- client/components/systems/impulse.js
+++ client/components/systems/impulse.js
@@ -16,7 +16,7 @@ module.exports = (io) => {
         }
         render(){
             return (
-                <input type="range" min="0" max="100" step="1" value={this.state.speed} onChange={(event)=>{this.setSpeed(parseFloat(event.target.value))}} class={styles.slider} />
+                <input type="range" min="0" max="100" step="1" value={this.state.speed} onChange={(event)=>{this.setSpeed(parseFloat(event.target.value))}} className={styles.slider} />
             )
         }
     }
