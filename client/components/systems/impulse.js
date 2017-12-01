9ac0d26eb558e0132ca8ec9dba90e3417896cf94
Trevor Jex <jextrevor@gmail.com>
2017-11-30 17:54:11 -0700

    Create impulse component

---
+++ client/components/systems/impulse.js
@@ -0,0 +1,24 @@
+"use strict";
+const React = require('react')
+const styles = require ('../../styles/systems/impulse.scss')
+
+module.exports = (io) => {
+    class Impulse extends React.Component {
+        constructor(props){
+            super(props)
+            this.state = {speed: 0}
+            io.on("Impulse.speed", (data) => {
+                this.setState({speed: data})
+            })
+        }
+        setSpeed(speed){
+            io.emit("Impulse.setSpeed",speed)
+        }
+        render(){
+            return (
+                <input type="range" min="0" max="100" step="1" value={this.state.speed} onChange={(event)=>{this.setSpeed(parseFloat(event.target.value))}} class={styles.slider} />
+            )
+        }
+    }
+    return Impulse
+}
